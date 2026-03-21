5def668ded59eb5b151ff45f747a706bf1ac8b48
Bibechana8 <bibechanapoudel25@gmail.com>
2026-03-21 17:54:24 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./MyComponents/Header";
 import Todos from "./MyComponents/Todos";
 import Footer from "./MyComponents/Footer";
 
 function App() {
-  let myvariable = "345";
+  
   return (
     <>
-    <Header/>
+    <Header title="My Todos List" searchBar={true}/>
     <Todos/>
     <Footer/>
     
--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Header() {
+
+export default function Header(props) {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">{props.title}</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -31,12 +33,20 @@ export default function Header() {
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
+      { props.searchBar? <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      </form>:""}
     </div>
   </div>
 </nav>
   )
+}
+Header.defaultProps = {
+  title: "Your Title Here",
+  searchBar: true
+}
+Header.propTypes = {
+  title: PropTypes.string,
+  searchBar: PropTypes.bool.isRequired
 }
